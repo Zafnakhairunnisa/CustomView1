746351253d64348e373eb03b594d702f721e5046
Zafnakhairunnisa <zafna.khairunnisa@mail.ugm.ac.id>
2023-06-21 16:06:17 +0700

    initial commit

---
+++ app/src/main/java/com/example/customview1/CustomButton.java
@@ -0,0 +1,42 @@
+package com.example.customview1;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class CustomButton extends AppCompatButton {
+
+    private boolean isColorChanged = false;
+
+    public CustomButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setBackgroundColor(Color.BLUE); // Warna awal button
+        setOnClickListener(v -> {
+            if (isColorChanged) {
+                setBackgroundColor(Color.BLUE); // Warna sesuai dengan kriteria
+                isColorChanged = false;
+            } else {
+                setBackgroundColor(Color.RED); // Warna berubah sesuai dengan kriteria
+                isColorChanged = true;
+            }
+        });
+    }
+
+}
